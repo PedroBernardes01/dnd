d5ebd52c0799293225686e144b8a97a0676e65e0
PedroBernardes01 <112957580+PedroBernardes01@users.noreply.github.com>
2023-02-10 13:01:28 -0300

    .

---
+++ crud/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react';
+
+import './index.css'
+
+let html = <div>
+    <h1>tst</h1>
+</div>;
+
+ReactDOM.render(
+    html
+    ,document.getElementById('root')
+);
