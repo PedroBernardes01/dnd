6a8417bcc1b7620bef397570f175edf192594c49
PedroBernardes01 <112957580+PedroBernardes01@users.noreply.github.com>
2023-02-13 15:29:04 -0300

    Update main.jsx

--- site/ClientApp/src/main.jsx
+++ site/ClientApp/src/main.jsx
@@ -1,8 +1,8 @@
-import { Button, Space } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
 
 export default function Main(){
     return (
-        <div><SearchOutlined/></div>
+        <div>
+            
+        </div>
     );
 };
